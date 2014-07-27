6e6e366e0725a29c9f2e59e3cc92db8111531be6
Cody Batt <cody.batt@software.dell.com>
2014-07-27 15:20:08 -0600

    start

---
+++ DisabledCommandTooltip.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Linq;
+using System.Net.Mime;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Input;
+using ReactiveUI;
+
+namespace Implbits
+{
+    public class DisabledCommandTooltip : DependencyObject
+    {
+        #region Attached Property: Enable
+        public static readonly DependencyProperty Enable = DependencyProperty.RegisterAttached(
+            "Enable",
+            typeof(Boolean),
+            typeof(DisabledCommandTooltip), new PropertyMetadata(false, EnableDynamicChanged)
+            );
+        public static void SetEnable(DependencyObject element, Boolean value)
+        {
+            element.SetValue(Enable, value);            
+        }
+        public static Boolean GetEnable(DependencyObject element)
+        {
+            return (Boolean)element.GetValue(Enable);
+        }
+
+        public static void EnableDynamicChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
+        {
+            if (args.NewValue is bool)
+            {
+                if ((bool)args.NewValue) WatchForCommandChange(obj as FrameworkElement);
+            }
+        }
+        #endregion
+
+        #region Private Attached Property: DefaultToolTip
+        private static readonly DependencyProperty DefaultToolTipProperty = DependencyProperty.RegisterAttached(
+            "DefaultToolTip",
+            typeof(object),
+            typeof(DisabledCommandTooltip)
+            );
+        private static void SetDefaultToolTip(FrameworkElement element, object value)
+        {
+            element.SetValue(DefaultToolTipProperty, value);
+        }
+        private static object GetDefaultToolTip(FrameworkElement element)
+        {
+            return element.GetValue(DefaultToolTipProperty);
+        }
+        #endregion
+
+        static readonly Dictionary<FrameworkElement, IDisposable> WatchRegistrations = new Dictionary<FrameworkElement, IDisposable>(); 
+        static void WatchForCommandChange(FrameworkElement element)
+        {
+            var cs = element as ICommandSource;
+            if (cs == null) throw new ArgumentException("DisabledCommandTooltip set on non-command source element: " + element);
+            if (cs.Command != null)
+            {
+                ConfigureDisabledTooltip(element);
+                return;
+            }
+            var subscription = cs.WhenAnyValue(x => x.Command).Subscribe(x =>
+            {
+                if (x == null) return;
+                if (WatchRegistrations.ContainsKey(element))
+                {
+                    var disp = WatchRegistrations[element];
+                    if (disp != null)
+                    {
+                        disp.Dispose();
+                    }
+                    WatchRegistrations.Remove(element);
+                }
+                ConfigureDisabledTooltip(element);
+            });
+            WatchRegistrations.Add(element, subscription);
+        }
+
+
+        static void ConfigureDisabledTooltip(FrameworkElement element)
+        {
+            var cs = element as ICommandSource;
+            if(cs == null) 
+                throw new ArgumentException("DisabledCommandTooltip set on non-CommandSource element: " + element);
+
+            var ixcommand = cs.Command as IXCommand;
+            if (ixcommand == null)
+            {
+                throw new ArgumentException("DisabledCommandTooltip.EnableDynamic is true, but command is not derived from IXCommand.");    
+            }
+
+            ToolTipService.SetShowOnDisabled(element, true);
+            SetDefaultToolTip(element, element.ToolTip ?? ToolTipService.GetToolTip(element));
+            UpdateTooltip(element, ixcommand.Reason);
+            ixcommand.CanExecuteChanged += (s, e) => UpdateTooltip(element,ixcommand.Reason);
+        }
+
+        static void UpdateTooltip(FrameworkElement element, string reason)
+        {
+            ToolTipService.SetToolTip(element, string.IsNullOrEmpty(reason) ? GetDefaultToolTip(element) : reason);
+        }
+    }
+}
---
+++ XCommand.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace Implbits
+{
+    public interface IXCommand : ICommand
+    {
+        string Reason { get; }
+    }
+
+    public class XCommand : IXCommand, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public event EventHandler CanExecuteChanged;
+
+        protected ICommand WrappedCommand { get; set; }
+        private readonly Func<object, string> _reasonCallback;
+
+        public XCommand(ICommand toWrap, Func<object, string> reasonCallback)
+        {
+            WrappedCommand = toWrap;
+            _reasonCallback = reasonCallback;
+            WrappedCommand.CanExecuteChanged += WrappedCommand_CanExecuteChanged;
+        }
+
+        void WrappedCommand_CanExecuteChanged(object sender, EventArgs e)
+        {
+            CanExecuteChanged(sender, e);
+        }
+
+        public XCommand(ICommand toWrap, string reason)
+        {
+            WrappedCommand = toWrap;
+            _reasonCallback = x=>reason;
+            WrappedCommand.CanExecuteChanged +=WrappedCommand_CanExecuteChanged;
+        }
+
+        private string _reason;
+        public string Reason
+        {
+            get { return _reason; }
+            protected set
+            {
+                if (_reason == value) return;
+                _reason = value;
+                FirePropertyChanged();
+                CanExecuteChanged(this, new EventArgs());
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            var canExecute = WrappedCommand.CanExecute(parameter);
+            Reason = canExecute ? null : _reasonCallback(parameter);
+            return canExecute;
+        }
+
+        public void Execute(object parameter)
+        {
+            WrappedCommand.Execute(parameter);
+        }
+        
+        void FirePropertyChanged([CallerMemberName] string property = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+        }
+
+
+        
+    }
+
+    public static class CommandExtensions
+    {
+        public static IXCommand WithDisabledTooltip(this ICommand command, Func<object,string> reasonCallback)
+        {
+            return new XCommand(command, reasonCallback);
+        }
+
+        public static IXCommand WithDisabledTooltip(this ICommand command, string reason)
+        {
+            return new XCommand(command, reason);
+        }
+    }
+
+}
